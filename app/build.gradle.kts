825d394daff6d94d031676def04ee50af2247440
Angelina Wei Yan Wu <angelinaweiyanwu@gmail.com>
2024-12-16 12:10:53 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.angelinaweiyanwu.currency_calculator"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.angelinaweiyanwu.currency_calculator"
-        minSdk = 35
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
