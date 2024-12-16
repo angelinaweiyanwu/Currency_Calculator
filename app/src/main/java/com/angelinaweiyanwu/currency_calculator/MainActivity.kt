c33f40c701627fe95d3f8ff053e8df2308adcf4c
Angelina Wei Yan Wu <angelinaweiyanwu@gmail.com>
2024-12-16 13:12:44 +0100

    Update MainActivity.kt

--- app/src/main/java/com/angelinaweiyanwu/currency_calculator/MainActivity.kt
+++ app/src/main/java/com/angelinaweiyanwu/currency_calculator/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.angelinaweiyanwu.currency_calculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,18 +21,17 @@ class MainActivity : AppCompatActivity() {
         }
         val spinner: Spinner = findViewById(R.id.spinner)
         //Adapter mit Array aus den Ressourcen
-        val adapter = ArrayAdapter.createFromResource(
+
+        val adapter = adapter.createFromResource(
             this,
             R.array.currency,
             R.layout.spinner_layout            )
         adapter.setDropDownViewResource(R.layout.spinner_layout)
         spinner.adapter = adapter
         val spinner2 : Spinner = findViewById(R.id.spinner2)
-        val adapter2 = ArrayAdapter.createFromResource(
-            this,            R.array.currency,            R.layout.pinner_layout        )
+        val adapter2 = adapter.createFromResource(
+            this,            R.array.currency,            R.layout.spinner_layout        )
         adapter.setDropDownViewResource(R.layout.spinner_layout)
         spinner2.adapter = adapter
-
-
     }
 }
