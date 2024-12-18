e5cd3b88749b048ee40a3cfca7ee03fb525c8bf2
Angelina Wei Yan Wu <angelinaweiyanwu@gmail.com>
2024-12-18 11:49:04 +0100

    Update ExchangeRateApi.kt

--- app/src/main/java/com/angelinaweiyanwu/currency_calculator/ExchangeRateApi.kt
+++ app/src/main/java/com/angelinaweiyanwu/currency_calculator/ExchangeRateApi.kt
@@ -1,9 +1,14 @@
 package com.angelinaweiyanwu.currency_calculator
 
-class ExchangeRateApi {
-    interface ExchangeRateApi {
-        @GET("v6/16d68c576083eb547208dbe0/latest/{currency}")
-        suspend fun getLatestRates(@Path("currency") baseCurrency: String): Response<ExchangeRateResponse>
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ExchangeRateApi {
+    @GET("v6/{16d68c576083eb547208dbe0}/latest/{currency}")
+    suspend fun getLatestRates(
+        @Path("16d68c576083eb547208dbe0") apiKey: String,
+        @Path("currency") baseCurrency: String
+    ): Response<ExchangeRateResponse>
     }
 
-}
